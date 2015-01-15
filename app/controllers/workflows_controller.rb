1bfac6cc455e605f66cec756e77c9cfc9ccd78c8
Adam Butler <adam@lab.io>
2015-01-15 23:09:47 +0000

    Add create action for workflows

--- app/controllers/workflows_controller.rb
+++ app/controllers/workflows_controller.rb
@@ -1,5 +1,5 @@
 class WorkflowsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_workflow, only: [:show]
   before_action :set_workflows, only: [:index]
 
@@ -15,6 +15,23 @@ class WorkflowsController < ApplicationController
       format.html
       format.json { render json: @workflow }
      end
+  end
+
+  def create
+    @workflow = Workflow.new(workflow_params)
+    @workflow.user_id = current_user.id
+    if @workflow.save
+      redirect_to @workflow
+    else
+      notice "Something went wrong"
+      redirect_to :back, status: 422
+    end
+  end
+
+  private
+
+  def workflow_params
+    params.require(:workflow).permit(:title, :description, :git_repository_url)
   end
 
   def set_workflow

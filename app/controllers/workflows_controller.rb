7d482599b2c307f3e40f47b620e569a44259994f
Adam Butler <adam@lab.io>
2015-01-15 23:08:57 +0000

    Add workflow#wshow

--- app/controllers/workflows_controller.rb
+++ app/controllers/workflows_controller.rb
@@ -1,10 +1,22 @@
 class WorkflowsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :set_workflow, only: [:show]
 
   def index
   end
 
   def new
     @workflow = Workflow.new
+  end
+
+  def show
+    respond_to do |format|
+      format.html
+      format.json { render json: @workflow }
+     end
+  end
+
+  def set_workflow
+    @workflow = Workflow.find(params[:id])
   end
 end

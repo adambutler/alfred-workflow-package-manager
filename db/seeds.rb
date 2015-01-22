0cf5fe0112b3707d12b0c944cfc4b94154325ced
Adam Butler <adam@lab.io>
2015-01-22 08:29:09 +0000

    More seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,35 @@ user = User.create!({
   password: "itsasecret"
 })
 
-workflow = Workflow.create!({
-  title: "html2haml",
-  git_repository_url: "git@github.com:adambutler/Alfred-Workflow-html2haml.git",
-  description: "Convert HTML to HAML right in your clipboard"
-})
+Workflow.create!([
+  {
+    title: "html2haml",
+    git_repository_url: "git@github.com:adambutler/Alfred-Workflow-html2haml.git",
+    description: "Convert HTML to HAML right in your clipboard",
+    user: user
+  },
+  {
+    title: "js2coffee",
+    git_repository_url: "git@github.com:asimpson/js2coffee-alfred-workflow.git",
+    description: "Converts copied JS to CoffeeScript and puts it on your clipboard",
+    user: user
+  },
+  {
+    title: "IMDB Search Suggest for Alfred",
+    git_repository_url: "git@github.com:mattvh/Alfred_IMDB_Suggest.git",
+    description: "Alfred workflow to add suggestions to IMDB searches",
+    user: user
+  },
+  {
+    title: "Dig",
+    git_repository_url: "git@github.com:phallstrom/AlfredDig.git",
+    description: "A workflow for Alfred to perform DNS lookups using dig and optionally copy a specific result to the clipboard.",
+    user: user
+  },
+  {
+    title: "YouTube",
+    git_repository_url: "git@github.com:simonbs/alfred-youtube-workflow.git",
+    description: "Search for YouTube videos and have them returned to Alfred.",
+    user: user
+  }
+])
